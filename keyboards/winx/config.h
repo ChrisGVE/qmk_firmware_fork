783e4b5ed54455500d6ce64ff285459ae627d116
Christian C. Berclaz <christian.berclaz@mac.com>
2023-02-01 19:36:26 +0100

    WindX (R1): Updated both the firmware and the standard layout

--- keyboards/winx/config.h
+++ keyboards/winx/config.h
@@ -3,30 +3,24 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x6060
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    windstudio
-#define PRODUCT         windX
-#define DESCRIPTION     Keyboard
+// #define VENDOR_ID       0xFEED
+// #define PRODUCT_ID      0x6060
+// #define DEVICE_VER      0x0001
+// #define MANUFACTURER    windstudio
+// #define PRODUCT         windX
 
 /* key matrix size */
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 18
-
 /* key matrix pins */
 #define MATRIX_ROW_PINS { F0, F1, D2, D1, D0 }
 #define MATRIX_COL_PINS { C7, C6, B6, E6, F6, D6, D7, B4, B5, F7, B7, F4, F5, D4, B0, B1, B2, B3 }
-#define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
 /* number of backlight levels */
-
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 0
-#endif
+/*  */
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCE 5
@@ -47,11 +41,3 @@
 /* prevent stuck modifiers */
 //#define PREVENT_STUCK_MODIFIERS
 
-
-#ifdef RGB_DI_PIN
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 0
-#define RGBLIGHT_HUE_STEP 8
-#define RGBLIGHT_SAT_STEP 8
-#define RGBLIGHT_VAL_STEP 8
-#endif
--- keyboards/winx/keymaps/chrisgve/keymap.c
+++ keyboards/winx/keymaps/chrisgve/keymap.c
@@ -4,10 +4,18 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
+	[_GAMING] = LAYOUT_tsangan(
+		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_PGUP,  KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGDN,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           END_ADJ,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,                      KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+		KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                    KC_RGUI,                   KC_LEFT, KC_DOWN, KC_RGHT,                    KC_P0,            KC_PDOT
+    ),
+
 	[_QWERTY_MAC] = LAYOUT_tsangan(
-		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_PGUP,  KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, CU_BSPC,          KC_PGDN,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-		CPS_CTL, A_MOUSE, KC_S,    M_NAV_1, M_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           END_F_M,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_PGUP,  KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGDN,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+		KC_LCTL, A_MOUSE, KC_S,    M_NAV_1, M_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           END_ADJ,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
 		SFT_MSE, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT,          KC_UP,                      KC_P1,   KC_P2,   KC_P3,   KC_PENT,
 		KC_LCTL, KC_LALT, KC_LGUI,                   SPC_RSE,                   KC_RGUI,                   KC_LEFT, KC_DOWN, KC_RGHT,                    KC_P0,            KC_PDOT
     ),
@@ -29,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
 	[_QWERTY_LINUX] = LAYOUT_tsangan(
-		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_PGUP,  KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, CU_BSPC,          KC_PGDN,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-		CPS_CTL, A_MOUSE, KC_S,    L_NAV_1, L_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           END_F_L,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_PGUP,  KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGDN,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+		KC_LCTL, A_MOUSE, KC_S,    L_NAV_1, L_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           END_ADJ,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
 		SFT_MSE, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT,          KC_UP,                      KC_P1,   KC_P2,   KC_P3,   KC_PENT,
 		KC_LCTL, KC_LGUI, KC_LALT,                   SPC_RSE,                   KC_RGUI,                   KC_LEFT, KC_DOWN, KC_RGHT,                    KC_P0,            KC_PDOT
     ),
@@ -53,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
 	[_QWERTY_WIN] = LAYOUT_tsangan(
-		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_PGUP,  KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
-		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, CU_BSPC,          KC_PGDN,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-		CPS_CTL, A_MOUSE, KC_S,    W_NAV_1, W_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           END_F_W,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,  KC_PGUP,  KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_PGDN,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+		KC_LCTL, A_MOUSE, KC_S,    W_NAV_1, W_F_NAV, KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           END_ADJ,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
 		SFT_MSE, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, R_SHIFT,          KC_UP,                      KC_P1,   KC_P2,   KC_P3,   KC_PENT,
 		KC_LCTL, KC_LGUI, KC_LALT,                   SPC_RSE,                   KC_RGUI,                   KC_LEFT, KC_DOWN, KC_RGHT,                    KC_P0,            KC_PDOT
     ),
@@ -77,34 +85,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     
 	[_RAISE] = LAYOUT_tsangan(
-		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______, _______,
-		_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, _______,           _______,  _______, _______, _______, _______,
+		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,  _______,  _______, _______, _______, _______,
+		_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, KC_DEL,            _______,  _______, _______, _______, _______,
 		KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,          KC_EQL,            _______,  _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                     _______, _______, _______, _______,
 		_______, _______, _______,                   _______,                            _______,                   _______, _______, _______,            _______,          _______ 
     ),
 
-	[_ADJUST_MAC] = LAYOUT_tsangan(
+	[_ADJUST] = LAYOUT_tsangan(
 		_______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,  _______,  _______, _______, _______, _______,
-		QK_BOOT, MAC_SWT, _______, WIN_SWT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,  _______, _______, _______, _______,
-		_______, DF_M_P,  DF_L_P,  DF_W_P,  _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,           _______,  _______, _______, _______, _______,
-		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, KC_MUTE, _______, _______, _______, KC_APFN,          KC_VOLU,                     _______, _______, _______, _______,
-		_______, _______, _______,                   KC_MPLY,                            _______,                   KC_MPRV, KC_VOLD, KC_MNXT,            _______,          _______ 
-    ),
-
-	[_ADJUST_LINUX] = LAYOUT_tsangan(
-		_______, KC_WF1,  KC_WF2,  KC_WF3,  KC_WF4,  KC_WF5,  KC_WF6,  KC_WF7,  KC_WF8,  KC_WF9,  KC_WF10, KC_WF11, KC_WF12, _______, _______,  _______,  _______, _______, _______, _______,
-		QK_BOOT, MAC_SWT, _______, WIN_SWT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,  _______, _______, _______, _______,
-		_______, DF_M_P,  DF_L_P,  DF_W_P,  _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,           _______,  _______, _______, _______, _______,
-		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, KC_MUTE, _______, _______, _______, INV_AFN,          KC_VOLU,                     _______, _______, _______, _______,
-		_______, _______, _______,                   KC_MPLY,                            _______,                   KC_MPRV, KC_VOLD, KC_MNXT,            _______,          _______ 
-    ),
-
-	[_ADJUST_WIN] = LAYOUT_tsangan(
-		_______, KC_WF1,  KC_WF2,  KC_WF3,  KC_WF4,  KC_WF5,  KC_WF6,  KC_WF7,  KC_WF8,  KC_WF9,  KC_WF10, KC_WF11, KC_WF12, _______, _______,  _______,  _______, _______, _______, _______,
-		QK_BOOT, MAC_SWT, _______, WIN_SWT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,  _______, _______, _______, _______,
-		_______, DF_M_P,  DF_L_P,  DF_W_P,  _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,           _______,  _______, _______, _______, _______,
-		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, KC_MUTE, _______, _______, _______, INV_AFN,          KC_VOLU,                     _______, _______, _______, _______,
+		QK_BOOT, MAC_SWT, _______, WIN_SWT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,            _______,  _______, _______, _______, _______,
+		KC_CAPS, DF_M_P,  DF_L_P,  DF_W_P,  _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______, _______,          _______,           _______,  _______, _______, _______, _______,
+		_______, DF_MAC,  DF_LNX,  DF_WIN,  _______, _______, _______, KC_MUTE, _______, _______, _______, _______,          KC_VOLU,                     _______, _______, _______, _______,
 		_______, _______, _______,                   KC_MPLY,                            _______,                   KC_MPRV, KC_VOLD, KC_MNXT,            _______,          _______ 
     ),
 
